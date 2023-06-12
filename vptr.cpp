e64e33bf6898586baac182f70770a139bbd1037e
bmatysiak <bartosz.matysiak@pwr.edu.pl>
2023-06-12 22:00:57 +0200

    Second commit

--- vptr.cpp
+++ vptr.cpp
@@ -24,15 +24,15 @@ int main()
   Base* b = &d;
   b -> a = 5;
   
-  std::cout << sizeof(int) << std::endl;
-  std::cout << sizeof(void*) << std::endl;
-  std::cout << sizeof(*b) << std::endl;
+  std::cout << "sizeof(int) == " << sizeof(int) << std::endl;
+  std::cout << "sizeof(void*) == " << sizeof(void*) << std::endl;
+  std::cout << "sizeof(*Base) == " << sizeof(*b) << std::endl;
   std::cout << "b -> a == " << b -> a << std::endl;
-  (b+1) -> a = 23777;
-  std::cout << "(xd!) b -> b == " << (b+1) -> a << std::endl;
-  std::cout << sizeof(d) << std::endl;
-  std::cout << d.a << std::endl;
-  std::cout << d.b << std::endl;
+  // (b+1) -> a = 23777;
+  // std::cout << "(xd!) b -> b == " << (b+1) -> a << std::endl;
+  std::cout << "sizeof(d) == " << sizeof(d) << std::endl;
+  std::cout << "d.a == " << d.a << std::endl;
+  std::cout << "d.b == " << d.b << std::endl;
   
   std::cout<<"///////////////////"<<std::endl;
   
@@ -43,7 +43,7 @@ int main()
   
   // std::cout << ((b-1)->a) << std::endl;
 
-  void* vptr = (void*)&d;
-  vptr=(void *)*vptr;
-  //std::cout << ((int (*)()) *vptr )() << std::endl;  
+  // void* vptr = (void*)&d;
+  // vptr=(void *)*vptr;
+  // std::cout << ((int (*)()) *vptr )() << std::endl;  
 }
